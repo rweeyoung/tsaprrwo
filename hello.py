74a5ecdbba879aab3ffa485f27997df735aefbb6
woon rwee young <woonrweeyoung@gmail.com>
2024-04-27 12:40:02 +0800

    start

--- hello.py
+++ hello.py
@@ -8,4 +8,4 @@ print("Random numbers:", random_numbers)
 sorted_numbers = sorted(random_numbers)
 print("Sorted numbers:", sorted_numbers)
 
-print("niggernigger")#i want to increasde it
+print("niggerniggernigger")#i want to increasde it
