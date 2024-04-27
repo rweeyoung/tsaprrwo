3d24732f8e52aabb3a0d4f6594084114378e301c
woon rwee young <woonrweeyoung@gmail.com>
2024-04-27 12:38:54 +0800

    establhsed

---
+++ hello.py
@@ -0,0 +1,11 @@
+import random
+
+# Generate a list of 10 random numbers between 1 and 100
+random_numbers = [random.randint(1, 100) for _ in range(10)]
+print("Random numbers:", random_numbers)
+
+# Sort the list of random numbers
+sorted_numbers = sorted(random_numbers)
+print("Sorted numbers:", sorted_numbers)
+
+print("nigger")
